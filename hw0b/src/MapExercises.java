15dfb0ee877b69b4635d707adc8309f12a8625d6
JasmineMace <1037547107@qq.com>
2025-07-27 15:21:33 +0800

    hw0b: Completed MapExercises

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
@@ -7,7 +9,13 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> treeMap = new TreeMap<>();
+        for (int i = 97; i <= 122; i++) {
+            char ch = (char) i; // translate ascii code to characters
+            treeMap.put(ch, i - 96);
+        }
+
+        return treeMap;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +23,29 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> squareMap = new TreeMap<>();
+        for (Integer num : nums) { // here you need to specify the type of elements that the collection contains in Java
+            int square = (int)Math.pow(num, 2); // the type of return value is double
+            squareMap.put(num, square);
+        }
+
+        return squareMap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> countMap = new TreeMap<>();
+        for (String word : words) {
+//            int sum = 0;
+//            for (String wordSub : words) {
+//                if (wordSub.contains(word)) {
+//                    sum++;
+//                }
+//            }
+//            countMap.put(word, sum);
+            countMap.put(word, countMap.getOrDefault(word, 0) + 1);
+        }
+        return countMap;
     }
 }
